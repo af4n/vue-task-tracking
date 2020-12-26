1a72715c6afa5cdde12368cac4262e063cc532ea
af4n <aleksey.afanaskin@gmail.com>
2020-12-26 11:34:11 +0200

    Initial dist subtree commit

---
+++ precache-manifest.e0141030ac092a773532200ff269176b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee8b5b598967b9f0214c",
+    "url": "/vue-task-tracking/css/app.9cea9b5f.css"
+  },
+  {
+    "revision": "49c0d8fd42d067f45260",
+    "url": "/vue-task-tracking/css/chunk-0e96a7ac.b44a6c86.css"
+  },
+  {
+    "revision": "11335b4aee5e8f6da82a",
+    "url": "/vue-task-tracking/css/chunk-vendors.24ce4a6c.css"
+  },
+  {
+    "revision": "d5e6fde3fb44747ac33cc28ce5231d80",
+    "url": "/vue-task-tracking/index.html"
+  },
+  {
+    "revision": "ee8b5b598967b9f0214c",
+    "url": "/vue-task-tracking/js/app.f9b0f2c9.js"
+  },
+  {
+    "revision": "49c0d8fd42d067f45260",
+    "url": "/vue-task-tracking/js/chunk-0e96a7ac.39d2be5e.js"
+  },
+  {
+    "revision": "d095e04476c7e798e12b",
+    "url": "/vue-task-tracking/js/chunk-2d0b64c8.bca14f03.js"
+  },
+  {
+    "revision": "837b6df35bf90589e941",
+    "url": "/vue-task-tracking/js/chunk-2d21ef2c.42521b9e.js"
+  },
+  {
+    "revision": "11335b4aee5e8f6da82a",
+    "url": "/vue-task-tracking/js/chunk-vendors.07a75579.js"
+  },
+  {
+    "revision": "07914f801ed14f8099622f7b1a1925b6",
+    "url": "/vue-task-tracking/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-task-tracking/robots.txt"
+  }
+]);
